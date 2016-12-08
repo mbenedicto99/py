05bf5c3795bbb3f78f6a9fc8bdb3314e548dc04e
root <mbenedicto@gmail.com>
2016-12-08 17:06:38 -0200

    commit20161208

--- updategit.sh
+++ updategit.sh
@@ -3,5 +3,5 @@ UPDATE=$(date +%Y%m%d)
 git init
 git add *
 git commit -m "commit${UPDATE}"
-git remote add origin https://github.com/mbenedicto99/py.git
+#git remote add origin https://github.com/mbenedicto99/py.git
 git push -u origin master
